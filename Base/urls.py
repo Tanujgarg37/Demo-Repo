cdb8b58a8522969e495e10fd222b00970f63c998
Tanuj Garg <tanujgarg37@gmail.com>
2024-07-19 18:42:58 +0530

    Adding Login Page

--- Base/urls.py
+++ Base/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path("", views.home, name="home"), 
 	path("projects/", views.projects, name="projects"), 
 	path("contact/", views.contact, name="contact"), 
+    path("login/",views.login_123,name='login')
 ]
--- Base/views.py
+++ Base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout 
-from .forms import SignupForm, LoginForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def home(request): 
@@ -12,3 +13,20 @@ def projects(request):
 def contact(request): 
 	return render(request, "contact.html")
 
+def login_123(request):
+	if request.method == 'POST':
+  
+        # AuthenticationForm_can_also_be_used__
+		username = request.POST['username']
+		password = request.POST['password']
+		print("username is ",username," password is ",password)
+		user = authenticate(request, username = username, password = password)
+		form_123=None
+		if user is not None:
+			form_123 = login(request, user)
+			messages.success(request, f' welcome {username} !!')
+			return redirect('home')
+		else:
+			messages.info(request, f'account done not exit plz sign in')
+	form_123 = AuthenticationForm()
+	return render(request, 'login.html', {'form':form_123,'title':"Login form"})
